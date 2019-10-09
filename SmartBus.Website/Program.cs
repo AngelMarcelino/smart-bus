20da3d3a385d7eaef97bc878e02c0c62288ebc43
Angel Marcelino González Mayoral <agemarcelino@hotmail.com>
2019-10-09 16:24:56 -0500

    Ensure migrated db before application start

--- SmartBus.Website/Program.cs
+++ SmartBus.Website/Program.cs
@@ -5,16 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SmartBus.Website.Data;
 
 namespace SmartBus.Website
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var webHost = CreateWebHostBuilder(args).Build();
+            var serviceScopeFactory = (IServiceScopeFactory)webHost.Services.GetService(typeof(IServiceScopeFactory));
+
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var dbContext = services.GetRequiredService<SmartBusDbContext>();
+
+                dbContext.Database.Migrate();
+            }
+            webHost.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
