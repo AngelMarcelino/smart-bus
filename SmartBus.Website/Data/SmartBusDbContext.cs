986d452c117c9916ca974812ea52afc6aa1ef073
Angel Marcelino González Mayoral <agemarcelino@hotmail.com>
2019-10-23 16:17:37 -0500

    Add identity migrations; Change DbContext to allow identity

--- SmartBus.Website/Data/SmartBusDbContext.cs
+++ SmartBus.Website/Data/SmartBusDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SmartBus.Website.Data.Entities;
 using System;
 using System.Collections.Generic;
@@ -7,16 +9,24 @@ using System.Threading.Tasks;
 
 namespace SmartBus.Website.Data
 {
-    public class SmartBusDbContext : DbContext
+    public class SmartBusDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
     {
         public SmartBusDbContext(DbContextOptions<SmartBusDbContext> options) : base(options)
         {
             
         }
         public DbSet<Bus> Buses { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Passage> Passages { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<UserCategory> UserCategories { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
