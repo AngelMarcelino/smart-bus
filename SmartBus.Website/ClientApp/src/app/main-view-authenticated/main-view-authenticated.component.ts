6f5fe9408ced3d8499c72915a1da90a1e1e1b1d8
Angel Marcelino González Mayoral <agemarcelino@hotmail.com>
2019-10-28 16:04:34 -0600

    Add main view for authenticated and no authenticated users, change structure of router outlets

---
+++ SmartBus.Website/ClientApp/src/app/main-view-authenticated/main-view-authenticated.component.ts
@@ -0,0 +1,9 @@
+import { Injectable, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view-authenticated.component.html'
+})
+export class MainViewAuthenticateComponent {
+
+}
---
+++ SmartBus.Website/ClientApp/src/app/main-view-no-authenticated/main-view-no-authenticated.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-view-no-auth',
+  templateUrl: 'main-view-no-authenticated.component.html'
+})
+export class MainViewNoAuthenticatedComponent {
+
+}
