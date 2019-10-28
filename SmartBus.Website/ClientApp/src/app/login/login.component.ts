e4c09d037f8f30cfe6373371527ae09004abe930
Angel Marcelino González Mayoral <agemarcelino@hotmail.com>
2019-10-28 16:03:48 -0600

    Add login component

---
+++ SmartBus.Website/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginModel } from '../models/login-model';
+import { AuthService } from '../services/auth.service';
+import { UiNotificationsService } from '../services/ui-notifications/ui-notifications.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.component.html'
+})
+export class LoginComponent {
+  formGroup: FormGroup;
+  showValidationError = false;
+  constructor(
+    formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private uiNotifications: UiNotificationsService) {
+      this.formGroup = formBuilder.group({
+        'email': ['', Validators.compose([Validators.required])],
+        'password': ['', Validators.compose([Validators.required])]
+      });
+  }
+  onFormSubmit() {
+    this.authService.login(<LoginModel>this.formGroup.value)
+      .subscribe( {
+        next: token => {
+          if (token) {
+            console.log(token);
+            this.router.navigate(['']);
+          } else {
+            this.showValidationError = true;
+          }
+        },
+        error: error => {
+          this.uiNotifications.launchErrorNotification('Usuario y/o contraseña incorrectos');
+        }
+      });
+  }
+
+}
