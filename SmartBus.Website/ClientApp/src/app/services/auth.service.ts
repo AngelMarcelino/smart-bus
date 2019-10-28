259ec42c32361416a7df5b198125dac5ee3ed0cd
Angel Marcelino González Mayoral <agemarcelino@hotmail.com>
2019-10-28 16:03:34 -0600

    Add auth service

---
+++ SmartBus.Website/ClientApp/src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, pipe } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LoginModel } from '../models/login-model';
+
+const AuthTokenKey = 'AuthToken';
+
+@Injectable()
+export class AuthService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  login(loginModel: LoginModel): Observable<string> {
+    return this.http.post('/api/Account/Login', loginModel, {
+      responseType: 'text'
+    })
+    .pipe(tap(token => {
+      localStorage.setItem(AuthTokenKey, token);
+    }));
+  }
+
+  isAuthenticated() {
+    if (localStorage.getItem(AuthTokenKey)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
